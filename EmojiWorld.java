5a284f90e3183ca7dd5573c08293db7f4b6fd093
Unknown <arthur.gho.howard@gmail.com>
2018-10-22 23:59:00 -0500

    Sprites added and objects created
    Added all the emojis we will be working with, and created default objects.

---
+++ EmojiWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class EmojiWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EmojiWorld extends World
+{
+
+    /**
+     * Constructor for objects of class EmojiWorld.
+     * 
+     */
+    public EmojiWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super( 960, 540, 1); 
+    }
+}
