93b0342528ce4f9c94fd8d23ad1cfa511d6194bc
Alexis Doll <alexis.dolly300@gmail.com>
2018-11-01 14:33:32 -0500

    Implemented attacking class

--- Attack.java
+++ Attack.java
@@ -8,6 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Attack extends Actor
 {
+    private int speed = -1;
+    private int damage= -1;
+    private EvilMojis target;
+    private String type;
     /**
      * Act - do whatever the Attack wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +19,37 @@ public class Attack extends Actor
     public void act() 
     {
         // Add your action code here.
-    }    
+    } 
+    public int getSpeed()
+    {
+        return speed;
+    }
+    public int getDamage()
+    {
+        return damage;
+    }
+    public EvilMojis getTarget()
+    {
+       return target; 
+    }
+    public String type()
+    {
+        return type;
+    }
+    public void setSpeed(int s)
+    {
+        speed= s;
+    }
+    public void setDamage(int d)
+    {
+        damage= d;
+    }
+    public void setTarget(EvilMojis e)
+    {
+        target= e;
+    }
+    public void setType(String t)
+    {
+        type=t;
+    }
 }
